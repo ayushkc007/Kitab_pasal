7ce95b860f238129d1c1bccacc9f817629b1bb7f
nabaraj82 <nabarajmagar54@gmail.com>
2021-04-24 10:57:36 +0545

    nabaraj third commit

--- ecommerce_project/ecommerce_project/settings.py
+++ ecommerce_project/ecommerce_project/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME' : 'bookstore',
         'USER' : 'postgres',
-        'PASSWORD' : 'password',
+        'PASSWORD' : 'xyz',
         'HOST' : '127.0.0.1',
         'PORT' : '5432',
     }
